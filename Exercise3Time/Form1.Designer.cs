347242191c306b84287e95d158e487152b8a6716
SReesberg <117655506+SReesberg@users.noreply.github.com>
2022-11-09 21:06:35 +0200

    Add files via upload

---
+++ Exercise3Time/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Exercise3Time
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(30, 38);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 25);
+            this.lblDate.TabIndex = 0;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(30, 91);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 25);
+            this.lblTime.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(35, 160);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(105, 44);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 226);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDate);
+            this.Name = "MainForm";
+            this.Text = "My Count Down";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Exercise3Time/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3Time
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.Date.ToString("dddd, MMMM, yyyy");
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int hours = DateTime.Now.Hour;
+            int minutes = DateTime.Now.Minute;
+            int seconds = DateTime.Now.Second;
+
+            if (hours == 15 && minutes == 30 && seconds == 00)
+            {
+                MessageBox.Show("It's exactly home time!", "Exact", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (hours > 15 && minutes > 30)
+            {
+                MessageBox.Show("It's time to go home.", "Late", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if(hours < 15)
+            {
+                MessageBox.Show("It's not home time yet.", "Early", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = "Time: " + DateTime.Now.ToString("h:mm:ss tt");
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Author: Stefan Reesberg \n Created: 19 October 2022", "Application Details", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
